7764318160eade523bec63d407f8263255ce0081
Adam <thomasadam83@hotmail.com>
2020-09-26 20:33:46 -0400

    Copied main code to solution file

--- links-list.php
+++ links-list.php
@@ -1,15 +1,7 @@
 <?php
 
 // Connect to the MySQL database
-$connect = mysqli_connect('localhost', 'root', 'root', 'sandbox');
 
-// If the connection did not work, display an error message
-if (!$connect) 
-{
-    echo 'Error Code: ' . mysqli_connect_errno() . '<br>';
-    echo 'Error Message: ' . mysqli_connect_error() . '<br>';
-    exit;
-}
 
 ?>
 <!doctype html>
@@ -24,75 +16,16 @@ if (!$connect)
         <?php
 
         // Create a query
-        $query = 'SELECT id,name,url,image,description
-            FROM links
-            ORDER BY name';
+        
 
         // Execute the query
-        $result = mysqli_query($connect, $query);
-
-        // If there is no result, display an error message
-        if (!$result)
-        {
-            echo 'Error Message: ' . mysqli_error($connect) . '<br>';
-            exit;
-        }
+        
 
         // Display the number of recirds found
-        echo '<p>The query found ' . mysqli_num_rows($result) . ' rows:</p>';
+        
 
         // Loop through the records found
-        while ($record = mysqli_fetch_assoc($result))
-        {
-
-            // Output the record using if statements and echo
-            echo '<hr>';
-
-            if ($record['name'])
-            {
-                echo '<h2>' . $record['name'] . '</h2>';
-            }
-            else
-            {
-                echo '<h2>' . $record['url'] . '</h2>';
-            }
-
-            if ($record['description'])
-            {
-                echo '<p>' . $record['description'] . '</p>';
-            }
-
-            if ($record['image'])
-            {
-                echo '<img src="' . $record['image'] . '" width="100">';
-            }
-
-            
-            if ($record['url'])
-            {
-
-                echo '<br>';
-
-                echo '<a href="' . $record['url'] . '">';
-
-                if ($record['name'])
-                {
-                    echo 'Visit ' . $record['name'];
-                }
-                else
-                {
-                    echo $record['url'];
-                }
-
-                echo '</a>';
-            }
-
-            // This code will help with debugging
-            echo '<pre>';
-            print_r($record);
-            echo '</pre>';
-
-        }
+        
 
         ?>
 
